282a88f8ff3dbca63c8c3913e096e9da28607d61
Jesse Eichar <jesse.eichar@camptocamp.com>
2011-09-07 22:32:06 +0200

    first commit

---
+++ 0.2.0-SNAPSHOT/js/projectProperties.js
@@ -0,0 +1,1 @@
+var IO_PROPS = {groupId: "com.github.scala-incubator.io",version: "0.2.0-SNAPSHOT",scalaVersion: "2.9.1",armVersion: "0.3",ioMavenPath: "com.github.scala-incubator.io".replace(/\./g,'/')};
