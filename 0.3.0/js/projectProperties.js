55d85d41fafebcc7b6838cec0be942f618010244
Jesse Eichar <jesse.eichar@camptocamp.com>
2011-12-31 11:40:43 +0100

    added 0.3.0 docs

---
+++ 0.3.0/js/projectProperties.js
@@ -0,0 +1,1 @@
+var IO_PROPS = {groupId: "com.github.scala-incubator.io",version: "0.3.0",scalaVersion: "2.9.1",armVersion: "1.1",ioMavenPath: "com.github.scala-incubator.io".replace(/\./g,'/'),SNAPSHOT_BLURB: ""};
